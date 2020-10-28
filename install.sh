43c74bdec2ead8dc5ffe40598b4360b0c5fc1a9d
David Ngo <DavidNgo.dev@gmail.com>
2020-10-29 08:21:01 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ sudo pacman -S xf86-video-fbdev xorg xorg-xinit nitrogen picom termite alacritty
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
 makepkg -si
-sudo pacman -S gnome gnome-extra
 echo "Done. You may want to reboot for everything to take affect."
