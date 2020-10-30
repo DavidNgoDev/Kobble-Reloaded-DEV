66a2d2216f849a970c13ee98878a9bb7bb4c5137
David Ngo <DavidNgo.dev@gmail.com>
2020-10-30 13:30:10 +1100

    Packages

---
+++ Theme.sh
@@ -0,0 +1,21 @@
+https://store.kde.org/p/1197198 | Bibata Cursor
+https://store.kde.org/p/1360254 | Oreo Cursors
+https://store.kde.org/p/1148692 | Capitaine Cursors
+https://store.kde.org/p/1365214 | Layan Cursors
+
+https://store.kde.org/p/1332404 | Flatery
+https://store.kde.org/p/1279924 | Tela Icon
+https://store.kde.org/p/1305429 | Mojave Circles
+https://store.kde.org/p/1305251 | Candy Icons
+https://store.kde.org/p/1238162 | Suru ++
+
+https://store.kde.org/s/KDE%20Store/p/1294729 | Sweet KDE
+https://store.kde.org/s/KDE%20Store/p/1354062 | ChromeOS
+https://store.kde.org/s/KDE%20Store/p/1325243 | Layan Theme
+https://store.kde.org/s/KDE%20Store/p/1400424 | MacOS Big Sur Theme
+
+https://store.kde.org/s/KDE%20Store/p/1270407/ | Rubberband Maximize Animation
+
+https://store.kde.org/s/KDE%20Store/p/1373008 | macOS Latte Dock
+https://store.kde.org/s/KDE%20Store/p/1354011 | Moe Layout
+https://store.kde.org/s/KDE%20Store/p/1399346 | macOS BigSur Dock
