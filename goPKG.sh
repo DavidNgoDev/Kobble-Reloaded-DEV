e535fd0b99fc2db51973e8b4a47d6f896465157c
David Ngo Dev <13045581+DavidNgoDev@users.noreply.github.com>
2020-11-04 16:38:15 +1100

    Update goPKG.sh

--- goPKG.sh
+++ goPKG.sh
@@ -34,6 +34,9 @@ sudo chmod a+wr /opt/spotify/Apps -R
 
 sudo pacman -Rs unzip
 
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
 sudo snap install spt
 sudo snap install code --classic
 sudo snap install notepadqq
