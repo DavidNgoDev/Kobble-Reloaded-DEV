415be51130844c74bc8ec5ad8e8d08939beb24df
David Ngo Dev <13045581+DavidNgoDev@users.noreply.github.com>
2020-11-04 16:36:23 +1100

    Update go.sh

--- go.sh
+++ go.sh
@@ -5,7 +5,7 @@ sudo pacman -S xf86-video-fbdev xorg xorg-xinit nitrogen picom alacritty firefox
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
 makepkg -si
-sudo pacman -S plasma plasma-wayland-session kde-application
+sudo pacman -S plasma plasma-wayland-session kde-applications
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
@@ -13,4 +13,5 @@ makepkg -si
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
+echo "Done. You may want to reboot for everything to take affect."
 reboot
