ad9ca6cd93a272bf39bd71c1533b868d3ba8fa17
David Ngo Dev <13045581+DavidNgoDev@users.noreply.github.com>
2020-11-04 16:33:34 +1100

    Update goPKG.sh

--- goPKG.sh
+++ goPKG.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
-sudo mkdir ~/Pictures/Wallpapers
-wget https://i.redd.it/587a3w75p1b51.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/cm2l9qq30yz41.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/rhu9xgqq9i351.jpg -P ~/Pictures/Wallpapers
-wget https://i.redd.it/nvsf29d135051.jpg -P ~/Pictures/Wallpapers
-wget https://i.redd.it/fiyjtl1rtoi51.jpg -P ~/Pictures/Wallpapers
-wget https://i.redd.it/3iwpgz1c9hh41.jpg -P ~/Pictures/Wallpapers
-wget https://i.redd.it/btqplv4tonv31.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/nvy8b1m6ii751.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/tizoc0t311c51.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/73mt4aji81831.png -P ~/Pictures/Wallpapers
-wget https://i.redd.it/qqjhrf0io4c51.png -P ~/Pictures/Wallpapers
-
 sudo mkdir ~/.local/share/fonts
-#cd ~/kobble-reloaded-dev/fonts/
-#tar -xvf fonts.tar.gz
-#rm fonts.tar.gz
-#cp -r ~/kobble-reloaded-dev/fonts/* ~/.local/share/fonts/
-#fc-cache -v
 
-sudo pacman -S alsa-utils pulseaudio pulseaudio-alsa maim feh xclip imagemagick blueman redshift xfce4-power-manager upower noto-fonts-emoji xdg-user-dirs iproute2 iw ffmpeg kitty neovim termite ncmpcpp mpd mpc lollypop vlc qutebrowser neofetch htop atom neovim kubectl gimp inkscape clamav clamtk net-tools arch-audit firewalld krita tmux virtualbox latte-dock plank awesome rofi
+sudo pacman -S alsa-utils pulseaudio cups pulseaudio-alsa unzip wget maim feh xclip chromium flatpak imagemagick blueman redshift xfce4-power-manager upower noto-fonts-emoji xdg-user-dirs iproute2 iw ffmpeg kitty neovim termite ncmpcpp mpd mpc lollypop vlc qutebrowser neofetch htop atom neovim kubectl gimp inkscape clamav clamtk net-tools arch-audit firewalld krita tmux virtualbox latte-dock plank awesome-git rofi pulseeffects
+sudo mkdir ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/587a3w75p1b51.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/cm2l9qq30yz41.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/rhu9xgqq9i351.jpg -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/nvsf29d135051.jpg -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/fiyjtl1rtoi51.jpg -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/3iwpgz1c9hh41.jpg -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/btqplv4tonv31.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/nvy8b1m6ii751.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/tizoc0t311c51.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/73mt4aji81831.png -P ~/Pictures/Wallpapers
+sudo wget https://i.redd.it/qqjhrf0io4c51.png -P ~/Pictures/Wallpapers
+sudo wget https://github.com/google/fonts/archive/master.zip -P ~/.local/share/fonts/
+cd ~/.local/share/fonts/
+sudo unzip master.zip
+cp -r /fonts-master/ofl* ~/.local/share/fonts/
+sudo rm master.zip
+sudo rm -r fonts-master
+
+flatpak install flathub com.spotify.Client
 
 yay -S spicetify-cli
+yay -S polybar
+yay -S nerd-fonts-complete
+
+sudo fc-cache -v
+
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
+
+sudo pacman -Rs unzip
 
 sudo snap install spt
 sudo snap install code --classic
